256f495dc93387aa5c3d37ac2bff1d14dd143f0d
Kr3Ked <_email@gmail.com>
2026-07-01 21:41:26 +0300

    Add TURN servers for calls

--- frontend/js/calls.js
+++ frontend/js/calls.js
@@ -18,15 +18,30 @@ const Calls = {
     currentFacingMode: 'user',
     incomingCallData: null,
     
-    rtcConfig: {
-        iceServers: [
-            { urls: 'stun:stun.l.google.com:19302' },
-            { urls: 'stun:stun1.l.google.com:19302' },
-            { urls: 'stun:stun2.l.google.com:19302' },
-            { urls: 'stun:stun3.l.google.com:19302' },
-            { urls: 'stun:stun4.l.google.com:19302' }
-        ]
-    },
+   rtcConfig: {
+    iceServers: [
+        { urls: 'stun:stun.l.google.com:19302' },
+        { urls: 'stun:stun1.l.google.com:19302' },
+        { urls: 'stun:stun2.l.google.com:19302' },
+        // Бесплатные публичные TURN серверы
+        {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        },
+        {
+            urls: 'turn:openrelay.metered.ca:443',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        },
+        {
+            urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        }
+    ],
+    iceCandidatePoolSize: 10
+},
 
     init() {
         if (!App.socket) {
